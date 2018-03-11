d21679efecdca830bf0aa304b797883fc9bef7e6
Administrator <Administrator@YLMF-1704081533>
2018-03-11 23:51:37 +0800

    监控服务

--- src/main/java/com/example/demo/VueController.java
+++ src/main/java/com/example/demo/VueController.java
@@ -1,35 +1,14 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
-@RestController
-@RefreshScope
+@Controller
 public class VueController {
 
-	@Value("${cloud.sample.msg:World}")
-	String msg;
-
-	@RequestMapping("/index")
+	@RequestMapping("/")
 	public String index() {
-		System.out.println(msg);
-		return msg;
+		return "forward:/hystrix";
 	}
-	@HystrixCommand(fallbackMethod="addback") //断路器注解
-	@RequestMapping("/add")
-	public String add() {
-		//模拟出错
-		int a=Integer.parseInt("ee");
-		a=a+1;
-		return "erro";
-	}
-	//备用方法
-	@RequestMapping("/addback")
-	public String addback() {
-		return "ok";
-	}
+	
 }
--- src/main/java/com/example/demo/WebApplication.java
+++ src/main/java/com/example/demo/WebApplication.java
@@ -2,17 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 /**
- * web服务
+ * 监控服务
  * @author mhs123
  *
  */
-@EnableZuulProxy
-@EnableHystrix
-@EnableDiscoveryClient
+@EnableHystrixDashboard
 @SpringBootApplication
 public class WebApplication {
 
